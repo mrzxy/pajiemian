5cb5f00b8aac84b743dc52a03dcfbaf432803215
zxy <zxy_fd139@163.com>
2025-03-16 15:49:09 +0800

    fix

--- helper.py
+++ helper.py
@@ -69,8 +69,8 @@ def capture_and_crop(region=None, save_path="screenshots"):
             img = img.crop((0, 0, region[2], region[3]))  # 裁剪
 
         now = datetime.now()
-        filename = os.path.join(save_path, "screenshot_{}.png".format(now.strftime("%Y%m%d_%H%M%S")))
-        img.save(filename)
+        filename = os.path.join(save_path, "screenshot_{}.pdf".format(now.strftime("%Y%m%d_%H%M%S")))
+        img.save(filename, "PDF", resolution=100.0)
         print(f"Saved: {filename}")
         return filename
     return None
