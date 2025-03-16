7650a4dcb7fee245195cbcbf79b4b3788df56f36
zxy <zxy_fd139@163.com>
2025-03-16 14:32:20 +0800

    fix

--- helper.py
+++ helper.py
@@ -1,11 +1,21 @@
 import base64
+from datetime import datetime
+
+import mss
+from PIL import Image
+import os
+import ctypes
+import sys
+
 
 def open_image_and_to_base64(image_path):
     with open(image_path, "rb") as img_file:
         return image_to_base64(img_file)
 
+
 def image_to_base64(img_file):
     return base64.b64encode(img_file.read()).decode("utf-8")
+
 
 def deep_get(d, keys, default=None):
     """ 使用递归方式获取嵌套字典中的值 """
@@ -14,6 +24,8 @@ def deep_get(d, keys, default=None):
     if not isinstance(d, dict):
         return default
     return deep_get(d.get(keys[0], default), keys[1:], default)
+
+
 def filter_nearest_less_equal(data, m):
     for offset in m:
         # 从后向前遍历
@@ -24,3 +36,51 @@ def filter_nearest_less_equal(data, m):
                 data.pop(i)
                 break
     return data
+
+
+def get_dpi_scale():
+    if sys.platform == "win32":
+        user32 = ctypes.windll.user32
+        user32.SetProcessDPIAware()  # 让 Python 进程感知 DPI
+        dpi = user32.GetDpiForSystem()
+        return dpi / 96.0  # 标准 DPI 为 96
+    else:
+        return 1.0
+
+
+def capture_and_crop(region=None, save_path="screenshots"):
+    """
+    定期截图并裁剪。
+    :param region: 截取区域 (left, top, width, height)，默认全屏
+    :param interval: 截图间隔（秒）
+    :param count: 截图次数
+    :param save_path: 图片保存路径
+    """
+    os.makedirs(save_path, exist_ok=True)
+
+    with mss.mss() as sct:
+        monitor = sct.monitors[1] if region is None else {"left": region[0], "top": region[1], "width": region[2],
+                                                          "height": region[3]}
+
+        screenshot = sct.grab(monitor)
+        img = Image.frombytes("RGB", screenshot.size, screenshot.rgb)
+
+        if region:
+            img = img.crop((0, 0, region[2], region[3]))  # 裁剪
+
+        now = datetime.now()
+        filename = os.path.join(save_path, "screenshot_{}.png".format(now.strftime("%Y%m%d_%H%M%S")))
+        img.save(filename)
+        print(f"Saved: {filename}")
+        return filename
+    return None
+
+
+def corp_image(image_path, region, save_path="screenshots"):
+    with Image.open(image_path) as img:
+        img = img.crop((region[0], region[1], region[2], region[3]))  # 裁剪
+        now = datetime.now()
+        filename = os.path.join(save_path, "corp_{}.png".format(now.strftime("%Y%m%d_%H%M%S")))
+        img.save(filename)
+        print(f"Saved: {filename}")
+        return filename
--- main.py
+++ main.py
@@ -1,58 +1,12 @@
-import json
-import time
-import mss
-from PIL import Image
-import os
-import ctypes
-import sys
-from AppKit import NSScreen
+
 import re
-from datetime import datetime
 from database import DB
 from dc import Discord
-from helper import image_to_base64, open_image_and_to_base64, deep_get, filter_nearest_less_equal
+from helper import image_to_base64, open_image_and_to_base64, deep_get, filter_nearest_less_equal, get_dpi_scale, \
+    capture_and_crop, corp_image
 
 from ocr_client import detect_text, mock_detect_text
 
-from glom import glom
-
-
-def get_dpi_scale():
-    if sys.platform == "win32":
-        user32 = ctypes.windll.user32
-        user32.SetProcessDPIAware()  # 让 Python 进程感知 DPI
-        dpi = user32.GetDpiForSystem()
-        return dpi / 96.0  # 标准 DPI 为 96
-    else:
-        return 1.0
-
-
-def capture_and_crop(region=None, save_path="screenshots"):
-    """
-    定期截图并裁剪。
-    :param region: 截取区域 (left, top, width, height)，默认全屏
-    :param interval: 截图间隔（秒）
-    :param count: 截图次数
-    :param save_path: 图片保存路径
-    """
-    os.makedirs(save_path, exist_ok=True)
-
-    with mss.mss() as sct:
-        monitor = sct.monitors[1] if region is None else {"left": region[0], "top": region[1], "width": region[2],
-                                                          "height": region[3]}
-
-        screenshot = sct.grab(monitor)
-        img = Image.frombytes("RGB", screenshot.size, screenshot.rgb)
-
-        if region:
-            img = img.crop((0, 0, region[2], region[3]))  # 裁剪
-
-        now = datetime.now()
-        filename = os.path.join(save_path, "screenshot_{}.png".format(now.strftime("%Y%m%d_%H%M%S")))
-        img.save(filename)
-        print(f"Saved: {filename}")
-        return filename
-    return None
 
 
 def match_result(resp):
@@ -127,14 +81,18 @@ def match_result(resp):
 
 
 def main():
-    scale = NSScreen.mainScreen().backingScaleFactor()
+    # scale = NSScreen.mainScreen().backingScaleFactor()
+    scale = 1
     scale_factor = get_dpi_scale()
     scaled_region = (
-        int(314 * scale_factor),  # left
-        int(158 * scale_factor),  # top
-        int(1150 * scale * scale_factor),  # width
-        int(680 * scale * scale_factor)  # height
+        int(0 * scale_factor),  # left
+        int(172 * scale_factor),  # top
+        int(946 * scale * scale_factor),  # width
+        int(651 * scale * scale_factor)  # height
     )
+    file = "/Users/zxy/Project/xianyu/capture/screenshots/screenshot_20250316_061851.png"
+    corp_image(file, scaled_region)
+    return
     scaled_region = None
     img_file = capture_and_crop(region=scaled_region)
     if img_file is None:
@@ -155,7 +113,6 @@ if __name__ == "__main__":
     db = DB()
     discord = Discord()
     interval = 1
-    # img_file = "/Users/zxy/Project/xianyu/capture/screenshots/large_text.png"
     # resp = detect_text(open_image_and_to_base64(img_file))
     # with open(os.path.join("case", "large_text.json"), "w") as f:
     #     f.write(resp.to_json_string())
