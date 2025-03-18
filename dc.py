88260728540dad978d1e9e1e03c6cbc6cd3c9cd4
zxy <zxy_fd139@163.com>
2025-03-18 17:49:10 +0800

    fix

--- dc.py
+++ dc.py
@@ -1,15 +1,15 @@
 import requests
 
-# webhooks = {
-#     "dp": "https://discord.com/api/webhooks/1351026063521611786/ocZvPs3Sj94Nye_bWOMlXfxv5XedwxQ_7yScMlvmarGI1-x5CzbTLbTNZUurm8NV5xmP",
-#     "rickman": "https://discord.com/api/webhooks/1351026161542627348/TrbGb9vI6DpglWEibiSCcDs49G7e7pNQBod1lulbsnwMes9thHB4hUHPRcsc5J2yCNNx",
-#     "kira": "https://discord.com/api/webhooks/1351026232753524858/cgHpgZ5CacN3fxb_Uxsz1RKmxWSnH7tlLwmP_BLA-fOsPTO1bpAU4mvRrbiOU3ik9VQY"
-# }
 webhooks = {
-    "dp": "https://discord.com/api/webhooks/1350049241501929542/whfC8x3c_NlUOyKwrayTFPs0NJRbDLJy_jtnkJU4zxSbh9UYmc-7fNCMUD5-2O3pclX-",
-    "rickman": "https://discord.com/api/webhooks/1350049241501929542/whfC8x3c_NlUOyKwrayTFPs0NJRbDLJy_jtnkJU4zxSbh9UYmc-7fNCMUD5-2O3pclX-",
-    "kira": "https://discord.com/api/webhooks/1350049241501929542/whfC8x3c_NlUOyKwrayTFPs0NJRbDLJy_jtnkJU4zxSbh9UYmc-7fNCMUD5-2O3pclX-"
+    "dp": "https://discord.com/api/webhooks/1351026063521611786/ocZvPs3Sj94Nye_bWOMlXfxv5XedwxQ_7yScMlvmarGI1-x5CzbTLbTNZUurm8NV5xmP",
+    "rickman": "https://discord.com/api/webhooks/1351026161542627348/TrbGb9vI6DpglWEibiSCcDs49G7e7pNQBod1lulbsnwMes9thHB4hUHPRcsc5J2yCNNx",
+    "kira": "https://discord.com/api/webhooks/1351026232753524858/cgHpgZ5CacN3fxb_Uxsz1RKmxWSnH7tlLwmP_BLA-fOsPTO1bpAU4mvRrbiOU3ik9VQY"
 }
+# webhooks = {
+#     "dp": "https://discord.com/api/webhooks/1350049241501929542/whfC8x3c_NlUOyKwrayTFPs0NJRbDLJy_jtnkJU4zxSbh9UYmc-7fNCMUD5-2O3pclX-",
+#     "rickman": "https://discord.com/api/webhooks/1350049241501929542/whfC8x3c_NlUOyKwrayTFPs0NJRbDLJy_jtnkJU4zxSbh9UYmc-7fNCMUD5-2O3pclX-",
+#     "kira": "https://discord.com/api/webhooks/1350049241501929542/whfC8x3c_NlUOyKwrayTFPs0NJRbDLJy_jtnkJU4zxSbh9UYmc-7fNCMUD5-2O3pclX-"
+# }
 class Discord:
     def __init__(self):
         pass
